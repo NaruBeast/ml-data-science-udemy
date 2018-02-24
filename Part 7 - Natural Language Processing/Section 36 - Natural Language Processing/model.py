a2f054c18b9f5cbd6d727accb042f442feba0c1f
NaruBeast <mohmahn@gmail.com>
2018-02-24 20:30:51 +0530

    NLP - 2. Cleaning corpus / Data preprocessing

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/model.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/model.py
@@ -4,3 +4,19 @@ import numpy as np
 
 import re
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
