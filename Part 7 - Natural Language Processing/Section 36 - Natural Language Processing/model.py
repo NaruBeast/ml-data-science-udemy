a9234d764bf96ed69056426d468a32a5f74b3270
NaruBeast <mohmahn@gmail.com>
2018-02-24 21:30:24 +0530

    NLP - 5. Naive Bayes Model Training

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/model.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/model.py
@@ -24,4 +24,17 @@ for i in range(0,1000):
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=1500)
 
-X = cv.fit_transform(corpus).toarray() 
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
