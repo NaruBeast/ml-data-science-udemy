a7f799bda62fa6a29bede7eeb38c53420d7a700e
NaruBeast <mohmahn@gmail.com>
2018-03-04 00:16:25 +0530

    CNN 6. Image Augmentation

--- Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/cnn.py
+++ Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/cnn.py
@@ -38,3 +38,23 @@ classifier.add(Dense(output_dim=1, activation='sigmoid'))
 
 #Compiling CNN
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+#Image Augmentation implemented to increase input images which reduces overfitting 
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
