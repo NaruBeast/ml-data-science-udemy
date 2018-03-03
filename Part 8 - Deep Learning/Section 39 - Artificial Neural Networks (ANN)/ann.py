6304de05e1affacdb7015180c326ed288fab1235
NaruBeast <mohmahn@gmail.com>
2018-03-03 17:42:38 +0530

    ANN - 3. Adding layers and compiling

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -26,4 +26,22 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+#Adding input and first hidden layer
+classifier.add(Dense(output_dim=10, init='uniform', activation='relu', input_dim=11))
+
+#Adding second hidden layer
+classifier.add(Dense(output_dim=6, init='uniform', activation='relu'))
+
+#Adding rhe output layer. 
+classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+
+#Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
 
